9de73ac1180d12f60e2a984837e7dd4da8270671
Harsh Upreti <98612598+harshu151103@users.noreply.github.com>
2025-04-29 15:20:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import FileUpload from './components/FileUpload';
 import SelectionPage from './components/SelectionPage';
 import ResultsTable from './components/ResultsTable';
+
+const ResultsWrapper = ({ results }) => {
+  const location = useLocation();
+  const numberOfDays = location.state?.numberOfDays || 0;
+  return <ResultsTable results={results} numberOfDays={numberOfDays} />;
+};
 
 const App = () => {
   const [results, setResults] = useState([]);
@@ -12,7 +18,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<FileUpload />} />
         <Route path="/select" element={<SelectionPage setResults={setResults} />} />
-        <Route path="/results" element={<ResultsTable results={results} />} />
+        <Route path="/results" element={<ResultsWrapper results={results} />} />
       </Routes>
     </Router>
   );
